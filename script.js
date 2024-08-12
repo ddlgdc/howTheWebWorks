8935776998bc15d3ce938430bc735ec674bba55e
ddlgdc <166570618+ddlgdc@users.noreply.github.com>
2024-08-12 15:26:54 -0700

    all answered

---
+++ script.js
@@ -0,0 +1,49 @@
+/* Part 1 
+HTTP
+    HyperText Trasnfer Protocol 
+
+URL
+    Uniform Resource Locator
+
+DNS
+    Domain Name System 
+
+Query String 
+    a part of a url used to pass parameters to the server 
+
+Two HTTP verbs 
+    GET and Post 
+    GET retreives data from the server 
+    POST submits data to the server to create or update a resource 
+
+HTTP request 
+    a message sent by a client (web browser) to a server to request data or 
+    to perform an action '
+    
+HTTP response 
+    the message sent by the server back to the client in reply to a HTTP 
+    request 
+
+HTTP header 
+    headers are used to pass additional information with request and responces
+
+    Request ex:
+    Accept: specifies the media types that the client can handle 
+    User-Agent: identifies the client software making the request 
+
+    Response ex: 
+    Content-Type: specifies the media type of the response conent 
+    Set-Cookie: used to sent cookies from the servier to the client 
+
+Processes when you type http://somesite.com/some/page.html in a browser 
+    URL parsing 
+    DNS resolution 
+    Connection establishment 
+    Sendning the HTTP request 
+    Server processing
+    Receiving the HTTP response 
+    Render the content 
+*/
+
+// Part 2
+
